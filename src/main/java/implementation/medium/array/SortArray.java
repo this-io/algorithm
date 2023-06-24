df2a24e84f64b83c80a1d126183173c30d14b3ca
suhas patil <suhas.patil@salesforce.com>
2023-06-24 13:18:54 -0500

    OrtArray

--- src/main/java/implementation/medium/array/SortArray.java
+++ src/main/java/implementation/medium/array/SortArray.java
@@ -44,6 +44,7 @@ public class SortArray {
         /*Copy data to temp arrays*/
         for (int i = 0; i < n1; ++i)
             L[i] = arr[l + i];
+
         for (int j = 0; j < n2; ++j)
             R[j] = arr[m + 1 + j];
 
